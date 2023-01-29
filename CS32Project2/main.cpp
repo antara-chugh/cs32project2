9c483e1f555262ac9e0b74c7af2abb88deb0e761
Antara <antarachugh@g.ucla.edu>
2023-01-29 10:25:58 -0800

    Initial Commit

---
+++ CS32Project2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CS32Project2
+//
+//  Created by Antara Chugh on 1/29/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
